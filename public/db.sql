efb6fd106624ee72b832da8709725233b2b4b3f4
eaterz <muraskoruslans@gmail.com>
2024-05-17 09:59:12 +0300

    sadaliju amatus

---
+++ public/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE noliktava;
+USE noliktava;
+INSERT INTO users (name, email, usertype, password) VALUES
+('ricards', 'ricards@gmail.com','admin', '$2y$12$erRq.o5vM59o9vAuLqCrs.77vicNZ245xf1rl2uRaO1hjTG6YZISe'),
+/*password: admin123*/
+('Harijs', 'harijs@gmail.com','noliktava', '$2y$12$jqOno3Sjj86HaCZQXUWOnOsIDLQVuEQPJvw9BmXEPolTNTGc5vOx2'),
+/*password: harijs123*/
+('Ruslans', 'rusis@gmail.com','plaukti', '$2y$12$1Gz/OfqDCoMQDkIWL8ItfOFbyQf3f1Ce4PtNkS6eqMlPv/tsbPKGG')
+/*password: rusis123 */
