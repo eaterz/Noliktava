a3e48277180d77bb8ba97c54a48684592bea117f
ricardsoss22 <bugats20fans@gmail.com>
2024-05-28 11:21:59 +0300

    uztaisiju style prieks user activity

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -1,17 +1,35 @@
 <x-musu.admin>
-    <h1 class=" text-3xl font-bold special-h1 normal-h1 text-center">Activity</h1>
-    <div class="flex justify-center items-center p-5">
-        <table class="rounded-lg bg-gray-200 ">
+<div class="flex flex-col items-center justify-center bg-white light:bg-zinc-900">
+    <h1 class="text-2xl font-bold mb-4 text-zinc-900 light:text-zinc-100">Activity</h1>
+    <div class="w-full max-w-4xl bg-white light:bg-zinc-800 shadow-md rounded-lg ">
+
+    <div class="flex justify-center items-center p-2">
+        <table class="min-w-full bg-white dark:bg-zinc-800 ">
+
+        <thead>
+                    <tr>
+                        <th class="px-4 py-2 text-left text-sm font-medium text-zinc-500 dark:text-zinc-400">User</th>
+                        <th class="px-4 py-2 text-left text-sm font-medium text-zinc-500 dark:text-zinc-400">Action</th>
+                        <th class="px-4 py-2 text-left text-sm font-medium text-zinc-500 dark:text-zinc-400">Description</th>
+                        <th class="px-4 py-2 text-left text-sm font-medium text-zinc-500 dark:text-zinc-400">Timestamp</th>
+                    </tr>
+        </thead>
+
             <tbody>
             @foreach($activities as $activity)
-                <tr>
-                    <td class="px-4 py-2 ">{{ $activity->user->name }}</td>
-                    <td class="px-4 py-2 ">{{ $activity->user->usertype }}</td>
-                    <td class="px-4 py-2 ">{{ $activity->description }}</td>
-                    <td class="px-4 py-2 ">{{ $activity->created_at }}</td>
+                <tr class="border-t border-zinc-200 dark:border-zinc-700">
+                    
+                    <td class="px-4 py-2 text-sm text-zinc-900 dark:text-zinc-100">{{ $activity->user->name }}</td>
+                    <td class="px-4 py-2 text-sm text-zinc-900 dark:text-zinc-100">{{ $activity->user->usertype }}</td>
+                    <td class="px-4 py-2 text-sm text-zinc-900 dark:text-zinc-100">{{ $activity->description }}</td>
+                    <td class="px-4 py-2 text-sm text-zinc-900 dark:text-zinc-100">{{ $activity->created_at }}</td>
                 </tr>
             @endforeach
             </tbody>
         </table>
     </div>
 </x-musu.admin>
+
+
+
+
