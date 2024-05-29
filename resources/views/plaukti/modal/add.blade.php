75a9fbae8ececa9cdade5cef58a95193a24ea50b
ricardsoss22 <bugats20fans@gmail.com>
2024-05-29 11:11:44 +0300

    uztaisiju mini stil nav bar turoinās next stundā

--- resources/views/plaukti/modal/add.blade.php
+++ resources/views/plaukti/modal/add.blade.php
@@ -6,7 +6,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-white">
-
+    
     <div class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50" id="addProductModal" style="display: none;">
             <div class="flex items-center justify-center h-screen">
              <div class="max-w-md rounded-lg bg-white p-6 shadow-lg">
